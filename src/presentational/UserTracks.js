541c7955de5af33cb30ca3774374ef4f3894f1e9
Anton Wallstedt <antonwallstedt@outlook.com>
2023-02-21 15:23:07 +0000

    Add user tracks presentational component

---
+++ src/presentational/UserTracks.js
@@ -0,0 +1,18 @@
+import Track from "../components/Track";
+
+function UserTracks({ userTracks, handleDelete }) {
+  return (
+    <ul className="user-tracks">
+      {allTracks.map(({ id, ref }) => (
+        <li key={id}>
+          <Track inputRef={ref} />
+          <button className="delete-btn" onClick={() => handleDelete(id)}>
+            X
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default UserTracks;
