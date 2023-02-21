c39d6e00f01fd2f26e73cc80396da48ef6651f93
Anton Wallstedt <antonwallstedt@outlook.com>
2023-02-21 16:27:09 +0000

    Add comments and implement hooks

--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -1,30 +1,67 @@
 import { useState } from "react";
-import * as WordSynth from "../utils/WordSynth";
+import WordSynth from "../utils/WordSynth";
+import { defaultScale } from "../utils/Scales";
+import NavBar from "../presentational/NavBar";
 import * as Tone from "tone";
 
-function NavBarContainer() {
-  /* Constants */
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+function NavBarContainer({ userTracks }) {
   var userInput = [];
+  var sequences = [];
+  const wordSynth = new WordSynth();
 
-  /* Hooks */
-  const handlePlay = () => {};
-  const handleStop = () => {};
+  /**
+   * Sets BPM and plays back user input
+   */
+  const handlePlay = () => {
+    Tone.Transport.bpm.value = bpm;
+    userInput = [];
+    for (const track of userTracks) {
+      userInput.push(track.ref.current.value);
+    }
+    playInput(userInput);
+  };
 
-  const [scale, setScale] = useState(null);
-  const handleScaleSelect = ({ target }) => {};
+  /**
+   * Disposes sequence(s) of user input.
+   */
+  const handleStop = () => {
+    for (const sequence of sequences) {
+      sequence.dispose();
+    }
+    sequences = [];
+  };
+
+  // TODO: Check how default init works...
+  const [scale, setScale] = useState(defaultScale);
+  /**
+   * Changes the scale used in `getNote`.
+   * @param {*} target the option the user has selected
+   */
+  const handleScaleSelect = ({ target }) => {
+    // When the scale is passed as a value-prop from the select option
+    // it doesn't return the array, instead a single line of all the notes
+    // separated by commas.
+    setScale(target.value.split(","));
+  };
 
   const [bpm, setBpm] = useState(120);
-  const handleBpmChange = ({ target }) => {};
+  /**
+   * Changes the bpm of `Tone.Transport`
+   * @param {*} target the target input field for setting BPM
+   */
+  const handleBpmChange = ({ target }) => {
+    setBpm(target.value);
+  };
 
   const playInput = (userInput) => {
-    let noteSequences = WordSynth.parseInput(userInput);
+    let noteSequences = wordSynth.parseInput(userInput, scale);
     const synth = new Tone.Synth();
     const freeverb = new Tone.Freeverb(0.2).toDestination();
     for (const i in noteSequences) {
       const seq = new Tone.Sequence((time, note) => {
         synth.triggerAttackRelease(note, 0.3, time).connect(freeverb);
       }, noteSequences[i]);
+      sequences.push(seq);
 
       // Start consecutive tracks i whole notes after previous track
       seq.start(i == 0 ? 0 : `${i}n`);
@@ -33,7 +70,14 @@ function NavBarContainer() {
   };
 
   // TODO: Return NavBar presentational component passing props as needed.
-  return;
+  return (
+    <NavBar
+      handlePlay={handlePlay}
+      handleStop={handleStop}
+      handleBpmChange={handleBpmChange}
+      handleScaleSelect={handleScaleSelect}
+    />
+  );
 }
 
 export default NavBarContainer;
