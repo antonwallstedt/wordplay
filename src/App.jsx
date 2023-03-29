a03cdb6bbc5718b73967ac237a433a32a33cb467
Anton Wallstedt <antonwallstedt@outlook.com>
2023-03-29 09:40:38 +0100

    Add play functionality

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,43 @@ function Track() {
     setText(target.value);
   };
 
+  const handlePlay = (input) => {
+    Tone.Transport.bpm.value = 90; // TODO: Allow setting this yourself
+    let noteSequence = wordSynth.parseInput(input, defaultScale);
+    const synth = new Tone.Synth();
+    const freeverb = new Tone.Freeverb(0.2).toDestination();
+    sequence = new Tone.Sequence((time, note) => {
+      synth.triggerAttackRelease(note, 0.3, time).connect(freeverb);
+      Tone.Draw.schedule(() => {
+        setCurrentWordIndex((currentIndex) => {
+          let nextIndex = (currentIndex + 1) % noteSequence.length;
+          return nextIndex;
+        });
+      }, time);
+    }, noteSequence);
+    sequence.start(0);
+    Tone.Transport.start();
+  };
+
+  const handleStop = () => {
+    setCurrentWordIndex(-1);
+    Tone.Transport.cancel();
+  };
+
+  // * Functions
+  const highlightWord = (word, index) => {
+    return (
+      <span
+        key={index}
+        style={spanStyle}
+        className={index === currentWordIndex ? "highlighted" : ""}
+      >
+        {word}
+      </span>
+    );
+  };
+
+  // * HTML
   return (
     <div className="track-container" style={divStyle}>
       <p style={paragraphStyle}>{text}</p>
