33c53e010a85e6e6161ddcb017e6cf7dbd17a8c0
antonwallstedt <antonwallstedt@outlook.com>
2023-04-07 11:46:47 +0200

    Cleanup and package installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import Playground from "./containers/Playground";
+import Sidebar from "./containers/Sidebar";
+import Toolbar from "./containers/Toolbar";
+
 function App() {
-  return <></>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const handleSidebarOpen = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <div className="flex flex-col overflow-x-hidden font-inter">
+      <Toolbar handleSideBarOpen={handleSidebarOpen} />
+      <div className="flex h-full flex-row overflow-x-hidden">
+        <Playground />
+        <Sidebar isShowing={sidebarOpen} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        jetbrains: ["JetBrains Mono", "monospace"],
+        caveat: ["Caveat", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
