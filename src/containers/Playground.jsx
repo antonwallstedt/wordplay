d2e3ce28c14d43615101f896d533cebf6a006c42
antonwallstedt <antonwallstedt@outlook.com>
2023-04-07 11:45:56 +0200

    Add Playground component

---
+++ src/containers/Playground.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// * This will be the container storing the tracks
+const Playground = () => {
+  return <div className="absolute z-0 h-full w-full p-20">Playground</div>;
+};
+
+export default Playground;
