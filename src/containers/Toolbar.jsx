62bd2dbdcbff0a1c9cef9e33f6eda5f864da3c70
antonwallstedt <antonwallstedt@outlook.com>
2023-04-12 11:45:23 +0200

    Add bpm functionality

--- src/containers/Toolbar.jsx
+++ src/containers/Toolbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { BiEdit } from "react-icons/bi";
 import ButtonPrimary from "../components/ButtonPrimary";
+import * as Tone from "tone";
 
-// * Toolbar at the top for media buttons among other things
-function Toolbar({ handleSideBarOpen }) {
+function Toolbar({ handleSideBarOpen, handlePlayAll, handleStopAll }) {
+  Tone.Transport.bpm.value = 90; // Default to 90 bpm
+  const handleBpmChange = ({ target }) => {
+    Tone.Transport.bpm.value = target.value;
+  };
   return (
     <div className="z-10 flex items-center justify-center bg-gradient-to-tr from-orange-200 to-orange-300 py-5 text-center drop-shadow-md">
       <h1 className="absolute left-10 font-caveat text-3xl font-bold drop-shadow-md md:text-4xl">
@@ -16,6 +20,7 @@ function Toolbar({ handleSideBarOpen }) {
         <input
           className="-ml-2 mt-1 h-7 w-12 rounded-md indent-1"
           placeholder="90"
+          onChange={handleBpmChange}
         />
       </div>
       <ButtonPrimary
