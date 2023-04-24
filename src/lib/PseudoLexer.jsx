c26046b19470582a0f46ab4a049992090367fe46
Anton Wallstedt <antonwallstedt@outlook.com>
2023-04-24 11:37:22 +0100

    Add velocity and duration

--- src/lib/PseudoLexer.jsx
+++ src/lib/PseudoLexer.jsx
@@ -7,21 +7,26 @@ class PseudoLexer {
     this.wordSynth = new WordSynth();
   }
 
-  interpret(userInput, scale) {
+  interpret(userInput, scale, octave) {
     let parsedInput = this.parser.parseInput(userInput);
     let result = [];
     let offset = 0;
     let rhythm = 0;
     for (var i = 0; i < parsedInput.length; i++) {
-      console.log(rhythm);
       let word = parsedInput[i].word;
       let cleanWord = parsedInput[i].cleanWord;
       if (parsedInput[i].comma) offset = 0.25;
-      if (parsedInput[i].semicolon) offset = 0.25;
+      if (parsedInput[i].semicolon) offset = 0.33;
       if (parsedInput[i].punctuation) offset = 0.5;
       result.push({
         word: word,
-        note: this.wordSynth.getNote(cleanWord, scale),
+        duration: cleanWord.length / 20,
+        velocity:
+          0.5 + (cleanWord.match(/[A-Z]/g) || "").length / cleanWord.length,
+        note: this.wordSynth.getNote(cleanWord, scale).replace(
+          /\d+/g, // Regex for replacing only the number in the string
+          String(octave)
+        ),
         time: rhythm,
       });
       rhythm += 0.25 + offset;
