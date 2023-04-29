1fa870c19284f08a365a3a6529407593abc7161a
Anton Wallstedt <antonwallstedt@outlook.com>
2023-04-29 14:20:23 +0100

    Modify synth instantiation

--- src/utils/Synths.jsx
+++ src/utils/Synths.jsx
@@ -1,21 +1,35 @@
 import * as Tone from "tone";
 
-const AM_SYNTH = new Tone.AMSynth();
-const DUO_SYNTH = new Tone.DuoSynth();
-const MEMBRANE_SYNTH = new Tone.MembraneSynth();
-const METAL_SYNTH = new Tone.MetalSynth();
-const MONO_SYNTH = new Tone.MonoSynth();
-const PLUCK_SYNTH = new Tone.PluckSynth();
-const SYNTH = new Tone.Synth();
+const AM_SYNTH = () => {
+  return new Tone.AMSynth();
+};
+const DUO_SYNTH = () => {
+  return new Tone.DuoSynth();
+};
+const MEMBRANE_SYNTH = () => {
+  return new Tone.MembraneSynth();
+};
+const METAL_SYNTH = () => {
+  return new Tone.MetalSynth();
+};
+const MONO_SYNTH = () => {
+  return new Tone.MonoSynth();
+};
+const PLUCK_SYNTH = () => {
+  return new Tone.PluckSynth();
+};
+const SYNTH = () => {
+  return new Tone.Synth();
+};
 
 // TODO: Fix issue with using same synth on multiple tracks!
 
 export const synths = [
-  { name: "AM", synth: AM_SYNTH },
-  { name: "Duo", synth: DUO_SYNTH },
-  { name: "Membrane", synth: MEMBRANE_SYNTH },
-  { name: "Metal", synth: METAL_SYNTH },
-  { name: "Mono", synth: MONO_SYNTH },
-  { name: "Pluck", synth: PLUCK_SYNTH },
-  { name: "Synth", synth: SYNTH },
+  { name: "AM", synth: AM_SYNTH() },
+  { name: "Duo", synth: DUO_SYNTH() },
+  { name: "Membrane", synth: MEMBRANE_SYNTH() },
+  { name: "Metal", synth: METAL_SYNTH() },
+  { name: "Mono", synth: MONO_SYNTH() },
+  { name: "Pluck", synth: PLUCK_SYNTH() },
+  { name: "Synth", synth: SYNTH() },
 ];
